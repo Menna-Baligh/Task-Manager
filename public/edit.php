859df2dbead138c033b590cdb998e5d5bf5764dc
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-04-13 21:03:10 +0200

    create style folder contains all css files

--- public/edit.php
+++ public/edit.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Task</title>
-    <link rel="stylesheet" href="edit.css">
+    <link rel="stylesheet" href="style/edit.css">
 </head>
 <body>
     <div class="container">
